cfb0ce4dbc7fce262ccc7f44dcbdee9cb2eb9599
ANIKINKIRILL <ky.anikin@mail.ru>
2019-06-23 21:30:53 +0300

    BaseApplication and AppComponent
    BaseApplication and AppComponent are working

---
+++ app/src/main/java/com/anikinkirill/instgraph/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.anikinkirill.instgraph;
+
+import com.anikinkirill.instgraph.dependencyinjection.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ app/src/main/java/com/anikinkirill/instgraph/dependencyinjection/AppComponent.java
@@ -0,0 +1,32 @@
+package com.anikinkirill.instgraph.dependencyinjection;
+
+import android.app.Application;
+
+import com.anikinkirill.instgraph.BaseApplication;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Singleton
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+
+    }
+
+}
