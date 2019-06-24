649fce4ab27fa9cd1c44bc2008e2db6b852c922d
ANIKINKIRILL <ky.anikin@mail.ru>
2019-06-24 19:24:48 +0300

    SessionManager saves user to cachedUser

--- app/src/main/java/com/anikinkirill/instgraph/SessionManager.java
+++ app/src/main/java/com/anikinkirill/instgraph/SessionManager.java
@@ -1,5 +1,14 @@
 package com.anikinkirill.instgraph;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+
+import com.anikinkirill.instgraph.models.User;
+
+import javax.inject.Inject;
+
 /**
  * This class keeps track of authenticated user
  * with the meta and current user data
@@ -7,4 +16,34 @@ package com.anikinkirill.instgraph;
  */
 
 public class SessionManager {
+
+    private MediatorLiveData<User> cachedUser = new MediatorLiveData<>();
+
+    @Inject
+    public SessionManager(){}
+
+    /**
+     * Authenticate user in ServiceDesk
+     * @param source   AuthApi.authenticateUser(login, password)...
+     */
+
+    public void authenticateUser(final LiveData<User> source){
+        cachedUser.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User user) {
+                cachedUser.setValue(user);
+                cachedUser.removeSource(source);
+            }
+        });
+    }
+
+    /**
+     * Get current authenticated user
+     * @return      current user
+     */
+
+    public LiveData<User> getAuthUser(){
+        return cachedUser;
+    }
+
 }
